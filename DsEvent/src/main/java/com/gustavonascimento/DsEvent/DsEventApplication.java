d725da70963b705a49245eebd75d8512d7163ead
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-01-21 17:07:24 -0300

    feature: Initial commit.

---
+++ DsEvent/src/main/java/com/gustavonascimento/DsEvent/DsEventApplication.java
@@ -0,0 +1,13 @@
+package com.gustavonascimento.DsEvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsEventApplication.class, args);
+	}
+
+}
